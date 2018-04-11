868ed8a301b25a5e75fbdb4918e179bfc8468c9a
Sam Getlan <samgetlan@gmail.com>
2018-04-11 16:27:50 -0700

    installed react and webpack, set up webpack.config file

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+import Community from './community';
+
+ReactDOM.render(<Community />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  devtool: "#eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  watch: true
+ };
