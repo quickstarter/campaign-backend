b60a5079fe2846a7b1449960dbe6fbcbbe380422
Sam Getlan <samgetlan@gmail.com>
2018-04-12 15:47:43 -0700

    Setup sass and styled totalBackersComponent

--- client/src/components/TotalBackers.jsx
+++ client/src/components/TotalBackers.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const TotalBackers = (props) => {
   return (
     <div className="TotalBackersContainer">
-      <div classNamt="title">
+      <div className="title">
         {props.totalBackers} people are supporting {props.projectCreator}
       </div>
     </div>
