59527745a3fd1dc6eb84c550ce48b4a8a5f71943
Sam Getlan <samgetlan@gmail.com>
2018-04-13 11:35:54 -0700

    Finished styling of NewAndOldBackers Component

--- client/src/components/NewAndOldBackers.jsx
+++ client/src/components/NewAndOldBackers.jsx
@@ -4,8 +4,8 @@ class NewAndOldBackers extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newBackers: 57,
-      oldBackers: 213
+      newBackers: 1057,
+      oldBackers: 2113
     };
   }
 
@@ -18,15 +18,17 @@ class NewAndOldBackers extends Component {
   render() {
     return (
       <div className="NewAndOldBackersContainer">
-        <div className="newBackersContainer">
-          <div className="subTitle">New Backers</div>
-          <div className="backerCount">{this.state.newBackers}</div>
-          <div className="supText">backers had never backed a project on Kickstarter before</div>
-        </div>
-        <div className="oldBackersContainer">
-          <div className="subTitle">Returning Backers</div>
-          <div className="backerCount">{this.state.oldBackers}</div>
-          <div className="supText">backers had backed a project on Kickstarter before</div>
+        <div className="NewAndOldBackersContent">
+          <div className="newBackersContainer">
+            <div className="subTitle">New Backers</div>
+            <div className="backerCount">{`${this.state.newBackers.toLocaleString('en', {useGrouping:true})}`}</div>
+            <div className="supText">backers had never backed a project on Kickstarter before</div>
+          </div>
+          <div className="oldBackersContainer">
+            <div className="subTitle">Returning Backers</div>
+            <div className="backerCount">{`${this.state.oldBackers.toLocaleString('en', {useGrouping:true})}`}</div>
+            <div className="supText">backers had backed a project on Kickstarter before</div>
+          </div>
         </div>
       </div>
     );
