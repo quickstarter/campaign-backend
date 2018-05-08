5c36c720013898d7036499128a57cde67595f8d7
Sam Liebow <sam.liebow@gmail.com>
2018-05-08 12:22:21 -0700

    Modify server to use PostgreSQL instead of MongoDB

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const utils = require('../db/utils.js');
 const cors = require('cors');
-
-// Middleware
 const parser = require('body-parser');
+const { Pool } = require('pg');
 
 const app = express();
-module.exports.app = app;
+
+const pool = new Pool({
+  user: 'samliebow',
+  password: 'sdc',
+  host: '127.0.0.1',
+  database: 'quickstarter',
+});
 
 // Set what we are listening on.
 app.set('port', (process.env.PORT || 3006));
@@ -27,18 +31,21 @@ app.use(cors());
 
 // Handle Get requests
 app.get('/api/community/:id', (req, res) => {
-  console.log('Handling get request');
-  utils.loadProject(req.params.id, (project) => {
-    console.log(project);
-    var project = project[0];
-    console.log('Retrieving backers from project:', project);
-    utils.loadBackers(project.backerIds, (backers) => {
-      res.json([project, backers]);
-    });
-  });
+  const { id } = req.params;
+  console.log(`Handling get request for project ${id}`);
+  pool.query(`SELECT * 
+    FROM quickstarter.projects 
+    INNER JOIN quickstarter.projects_backers 
+      ON quickstarter.projects.id = quickstarter.projects_backers.project
+    INNER JOIN quickstarter.users
+      ON quickstarter.projects_backers.backer = quickstarter.users.id
+    WHERE quickstarter.projects.id = $1::integer;`, [id])
+    .then((result) => {
+      const { title, creator, backers } = result.rows[0];
+      const project = { title, creator, backers };
+      res.json([project, result.rows]);
+    })
+    .catch(err => console.error(err));
 });
 
-if (!module.parent) {
-  app.listen(app.get('port'));
-  console.log('Listening on', app.get('port'));
-}
+app.listen(app.get('port'), () => console.log('Listening on', app.get('port')));
