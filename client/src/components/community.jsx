3825fd8fbb72f97cc92e56e47b7caa6709371614
Sam Liebow <sam.liebow@gmail.com>
2018-05-11 20:29:02 -0700

    Include CSS styles in component, remove data request on mount

--- client/src/components/community.jsx
+++ client/src/components/community.jsx
@@ -5,44 +5,33 @@ import BackersCities from './BackersCities.jsx';
 import BackersCountries from './BackersCountries.jsx';
 import NewAndOldBackers from './NewAndOldBackers.jsx';
 import RollCall from './RollCall.jsx';
+import styles from '../stylesheets/base.scss';
 
+const CSS = styles.toString();
 
 class Community extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      creator: '',
-      backers: [],
+      title: props.data.project.title,
+      creator: props.data.project.creator,
+      backers: props.data.backers,
     };
   }
 
-  componentDidMount() {
-    const context = this;
-    axios.get(`http://127.0.0.1:3006/api/community/${this.props.projectId || 0}`)
-      .then((response) => {
-        context.setState({
-          title: response.data[0].title,
-          creator: response.data[0].creator,
-          backers: response.data[1],
-        });
-      })
-      .catch((error) => {
-        console.log('There was an error fetching this project:', error);
-      });
-  }
-
-
   render() {
     return (
-      <div className="communityModuleContainer">
-        <TotalBackers totalBackers={this.state.backers.length} projectCreator={this.state.creator} />
-        <div className="communityLocationDataContainer">
-          <BackersCities backers={this.state.backers} />
-          <BackersCountries backers={this.state.backers} />
+      <div>
+        <style type="text/css">{CSS}</style>
+        <div className="communityModuleContainer">
+          <TotalBackers totalBackers={this.state.backers.length} projectCreator={this.state.creator} />
+          <div className="communityLocationDataContainer">
+            <BackersCities backers={this.state.backers} />
+            <BackersCountries backers={this.state.backers} />
+          </div>
+          <NewAndOldBackers backers={this.state.backers} />
+          <RollCall backers={this.state.backers} projectTitle={this.state.title} />
         </div>
-        <NewAndOldBackers backers={this.state.backers} />
-        <RollCall backers={this.state.backers} projectTitle={this.state.title} />
       </div>
     );
   }
